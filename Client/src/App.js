f08dfdb393e20fc6c24f67da8a02abb110f6a604
ryanpatrck <ryanpatrck@gmail.com>
2018-07-29 13:06:04 -0400

    heroku

--- Client/src/App.js
+++ Client/src/App.js
@@ -7,6 +7,20 @@ import Home from './Pages/Home'
 
 
 class App extends Component {
+ /* Create a new instance of the 'Authservice' compoenent*/
+ Auth = new AuthService();
+
+  state = {
+    username: "",
+    password: ""
+}
+
+/* Here will want to add a method to log the user out upon clicking 'Logout' */
+  _handleLogout = () => {
+    //include the logout() method from our AuthService helper class here.
+    this.Auth.logout();
+    this.props.history.replace('/login');
+  }
 
   render() {
 

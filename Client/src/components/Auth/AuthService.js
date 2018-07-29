f06dedf34f81ab7be0f86cee89354bd02ce214a5
ryanpatrck <ryanpatrck@gmail.com>
2018-07-29 12:38:35 -0400

    heroku

--- Client/src/components/Auth/AuthService.js
+++ Client/src/components/Auth/AuthService.js
@@ -1,82 +1,68 @@
 import decode from 'jwt-decode';
 export default class AuthService {
-    // Initializing important variables
-    constructor(domain) {
-        this.fetch = this.fetch.bind(this) // React binding stuff
-        this.login = this.login.bind(this)
-        this.getProfile = this.getProfile.bind(this)
-    }
 
-    login(username, password) {
-        // Get a token from api server using the fetch api
+    login = (email, password) => {
+        console.log("In login")
         return this.fetch('/login', {
             method: 'POST',
             body: JSON.stringify({
-                username,
+                email,
                 password
             })
         }).then(res => {
             console.log("setting token because we're awesome")
-            this.setToken(res.token) // Setting the token in localStorage
+            this.setToken(res.token)
             return Promise.resolve(res);
         })
     }
 
-    loggedIn() {
-        // Checks if there is a saved token and it's still valid
-        const token = this.getToken() // GEtting token from localstorage
-        return !!token && !this.isTokenExpired(token) // handwaiving here
+    loggedIn = () => {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
     }
 
-    isTokenExpired(token) {
+    isTokenExpired = (token) => {
         try {
             const decoded = decode(token);
-            if (decoded.exp < Date.now() / 1000) { // Checking if token is expired. N
+            if(decoded.exp < Date.now() / 1000) {
                 return true;
             }
-            else
+            else{
                 return false;
+            }
         }
-        catch (err) {
+        catch(err) {
+            console.log(err)
             return false;
         }
     }
 
-    setToken(idToken) {
-        // Saves user token to localStorage
+    setToken = (idToken) => {
         localStorage.setItem('id_token', idToken)
     }
 
-    getToken() {
-        // Retrieves the user token from localStorage
+    getToken = () => {
         return localStorage.getItem('id_token')
     }
 
-    logout() {
-        // Clear user token and profile data from localStorage
-        localStorage.removeItem('id_token');
+    logout = () => {
+        localStorage.removeItem('id_token')
     }
 
-    getProfile() {
-        // Using jwt-decode npm package to decode the token
-        return decode(this.getToken());
+    getConfirm = () => {
+        let answer = decode(this.getToken());
+        return answer;
     }
 
-
-    fetch(url, options) {
-        // performs api calls sending the required authentication headers
+    fetch = (url, options) => {
         const headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
-
-        // Setting Authorization header
-        // Authorization: Bearer xxxxxxx.xxxxxxxx.xxxxxx
-        if (this.loggedIn()) {
-            headers['Authorization'] = 'Bearer ' + this.getToken()
+        if(this.loggedIn()) {
+            headers['Authorization'] = 'Bearer' + this.getToken();
         }
-
-        return fetch(url, {
+        return this.fetch(url, {
             headers,
             ...options
         })
@@ -84,13 +70,13 @@ export default class AuthService {
             .then(response => response.json())
     }
 
-    _checkStatus(response) {
-        // raises an error in case response status is not a success
-        if (response.status >= 200 && response.status < 300) { // Success status lies between 200 to 300
-            return response
-        } else {
+    _checkStatus = (response) => {
+        if(response.status >= 200 && response.status <300) {
+            return response;
+        }
+        else {
             var error = new Error(response.statusText)
-            error.response = response
+            error.response = response;
             throw error
         }
     }

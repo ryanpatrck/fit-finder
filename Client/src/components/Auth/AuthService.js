69258bc1b2509eeb1245493edd43dbde85e175f9
ryanpatrck <ryanpatrck@gmail.com>
2018-07-29 14:35:33 -0400

    heroku

--- Client/src/components/Auth/AuthService.js
+++ Client/src/components/Auth/AuthService.js
@@ -1,8 +1,10 @@
 import decode from 'jwt-decode';
 export default class AuthService {
     // Initializing important variables
+    
     constructor(domain) {
-        this.domain = domain || "https://rossfitfinder.herokuapp.com/"// API server domain
+        const PORT = process.env.PORT || 3001
+        this.domain = domain || PORT// API server domain
         this.fetch = this.fetch.bind(this) // React binding stuff
         this.login = this.login.bind(this)
         this.getProfile = this.getProfile.bind(this)

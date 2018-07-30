4fe4ce3e6a3cb2186f5acada54cea00d16e1aaff
ryanpatrck <ryanpatrck@gmail.com>
2018-07-29 21:25:01 -0400

    heroku

--- server_official.js
+++ server_official.js
@@ -120,18 +120,18 @@ app.get('/', jwtMW /* Using the express jwt MW here */, (req, res) => {
     res.send('You are authenticated'); //Sending some response when authenticated
 });
 
-// // Error handling 
-// app.use(function (err, req, res, next) {
+// Error handling 
+app.use(function (err, req, res, next) {
   
-//     if (err.name === 'UnauthorizedError') { // Send the error rather than to show it on the console
-//         res.status(401).send(err);
-//         console.log("I am not working!!!!", err)
+    if (err.name === 'UnauthorizedError') { // Send the error rather than to show it on the console
+        res.status(401).send(err);
+        console.log("I am not working!!!!", err)
         
-//     }
-//     else {
-//         next(err);
-//     }
-// });
+    }
+    else {
+        next(err);
+    }
+});
 
 
 

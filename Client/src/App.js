3ace6fe411ae369ef613821415370e1f95f0ab34
ryanpatrck <ryanpatrck@gmail.com>
2018-07-31 10:58:07 -0400

    heroku

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import AuthService from './components/Auth/AuthService';
-import withAuth from './components/Auth/withAuth';
-import Home from './Pages/Home'
+import AuthService from './components/AuthService';
+import withAuth from './components/withAuth';
+
 
 
 
 class App extends Component {
 
+  /* Create a new instance of the 'Authservice' compoenent*/
+ Auth = new AuthService();
+
+  state = {
+    username: "",
+    password: ""
+}
+
+/* Here will want to add a method to log the user out upon clicking 'Logout' */
+  _handleLogout = () => {
+    //include the logout() method from our AuthService helper class here.
+    this.Auth.logout();
+    this.props.history.replace('/login');
+  }
+
+  //Render the protected component
   render() {
-    console.log('I was triggered during render')
+
+    let name = this.props.confirm.name;
+
     return (
-     
-      <div>
-      <div className="main-page">
+      <div className="App">
+        <div className="main-page">
           <div className="top-section">
             <h1>Welcome, {name}</h1>
           </div>
@@ -22,11 +38,10 @@ class App extends Component {
           </div>
         </div>
       </div>
-
     );
   }
-
 }
 
-
+//In order for this component to be protected, we must wrap it with what we call a 'Higher Order Compoenent' or HOC.
 export default withAuth(App);
+
